50adff1533a81399db25a622db514c0196fb4356
pfreires@ifes.edu.br <pfreires@.cefetes.br>
2010-12-07 11:11:58 -0200

    Adiciona projeto eventex da aula 1

---
+++ core/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class HomepageUrlTest(TestCase):
+    def test_success_when_get_homepage(self):
+        response = self.client.get('/')
+        self.assertEquals(200, response.status_code)
+        self.assertTemplateUsed(response, 'index.html')
+
